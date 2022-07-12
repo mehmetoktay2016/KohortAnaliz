475ad078c95702f12d8dde3318fd8ad82080dcae
mehmetoktay2016 <52069209+mehmetoktay2016@users.noreply.github.com>
2022-07-12 12:01:17 +0300

    Add files via upload

---
+++ kohort_analizi.sql
@@ -0,0 +1,47 @@
+-- KOHORT ANALÝZÝ
+
+SELECT
+                customer_month_count.cohort_month,
+                customer_month_count.order_month,
+                COUNT(DISTINCT customer_month_count.CustomerID) CustomerCounts,
+                DATEDIFF(month, customer_month_count.cohort_month , customer_month_count.order_month) PeriodNumber
+FROM
+
+
+(
+SELECT 
+	CustomerID, cohort_month,  order_month 
+FROM
+	(SELECT 
+		CustomerID,
+		 DATEFROMPARTS(
+						  YEAR(InvoiceDate)
+						  ,MONTH(InvoiceDate)
+						  ,1) order_month
+	
+	FROM 
+		[dbo].[online_retail_new_]
+	WHERE 
+		CustomerID IS NOT NULL) order_
+INNER JOIN
+(
+
+SELECT 
+		CustomerID  CustomerID_cohort,
+		 DATEFROMPARTS(
+						  YEAR(MIN(InvoiceDate))
+						  ,MONTH(MIN(InvoiceDate))
+						  ,'01') cohort_month
+	
+	FROM 
+		[dbo].[online_retail_new_]
+	GROUP BY
+		CustomerID
+
+) cohort ON order_.CustomerID = cohort.CustomerID_cohort
+) customer_month_count
+
+GROUP BY
+                customer_month_count.cohort_month,
+                customer_month_count.order_month
+
